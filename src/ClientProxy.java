e4f5651df07c92ae2dc2b3bb4ec764a05e405c16
Gavin McGuire <mcguireg@unm.edu>
2019-04-11 14:42:24 -0600

    ripped some code for the proxys

--- src/ClientProxy.java
+++ src/ClientProxy.java
@@ -1,10 +1,71 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ClientProxy {
 
-    ClientProxy(String hostName, int portNumber) throws IOException {
+    // initialize socket and input output streams
+    private Socket socket            = null;
+    private DataInputStream  input   = null;
+    private DataOutputStream out     = null;
 
-        Socket socket = new Socket(hostName, portNumber);
+    // constructor to put ip address and port
+    public ClientProxy(String address, int port)
+    {
+        // establish a connection
+        try
+        {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            // takes input from terminal
+            input  = new DataInputStream(System.in);
+
+            // sends output to the socket
+            out    = new DataOutputStream(socket.getOutputStream());
+        }
+        catch(UnknownHostException u)
+        {
+            System.out.println(u);
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+
+        // string to read message from input
+        String line = "";
+
+        // keep reading until "Over" is input
+        while (!line.equals("Over"))
+        {
+            try
+            {
+                System.out.println("type a message to server:");
+                line = input.readLine();
+                out.writeUTF(line);
+            }
+            catch(IOException i)
+            {
+                System.out.println(i);
+            }
+        }
+
+        // close the connection
+        try
+        {
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        ClientProxy client = new ClientProxy("127.0.0.1", 5000);
     }
 }
