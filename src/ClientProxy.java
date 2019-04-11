5216c90bf24644afc1fef5edf197869faff57264
Gavin McGuire <mcguireg@unm.edu>
2019-04-11 11:21:41 -0600

    server com classes

--- src/ClientProxy.java
+++ src/ClientProxy.java
@@ -1,2 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClientProxy {
+
+    public static void main(String[] args) throws IOException {
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+
+        try (Socket socket = new Socket(hostName, portNumber);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            String fromServer = in.readLine();
+            while (fromServer != null) {
+
+                System.out.println("Server: " + fromServer);
+                if (fromServer.equals("Bye.")) {
+                    break;
+                }
+
+                String fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Client: " + fromUser);
+                    out.println(fromUser);
+                }
+                fromServer = in.readLine();
+            }
+        }
+    }
 }
--- src/ServerProxy.java
+++ src/ServerProxy.java
@@ -1,2 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ServerProxy {
+
+    public static void main(String[] args) throws IOException {
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        ServerSocket serverSocket = new ServerSocket(portNumber);//  Listen  for  new  clients  forever
+        while (true) {//  Create  new  thread  to  handle  each  clientSocket
+            ClientProxy clientProxy = serverSocket.accept();
+            Bid kk = new Bid(clientProxy);
+            Thread t = new Thread(kk);
+            t.start();
+        }
+    }
 }
