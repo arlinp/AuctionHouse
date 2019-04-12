ce1a860196a41b68b769c84587f07f5312005de6
frostc <frostc@unm.edu>
2019-04-12 10:46:36 -0600

    Implemented Methods

--- src/AuctionHouse.java
+++ src/AuctionHouse.java
@@ -1,2 +1,37 @@
-public class AuctionHouse {
+import java.util.ArrayList;
+
+public class AuctionHouse implements AuctionProcess{
+
+
+    /**
+     * To place a bid
+     *
+     * @param bid    Bid object that contains elements
+     * @param itemID Identifier of Item
+     */
+    @Override
+    public void bid(Bid bid, int itemID) {
+
+    }
+
+    /**
+     * Gets the Item Info from the Item ID.
+     *
+     * @param itemID Identifier of Item
+     * @return ItemInfo
+     */
+    @Override
+    public ItemInfo getItemInfo(int itemID) {
+        return null;
+    }
+
+    /**
+     * Gets an ArrayList of all of the Items
+     *
+     * @return ArrayList of Items
+     */
+    @Override
+    public ArrayList<ItemInfo> getItems() {
+        return null;
+    }
 }
--- src/BankApp.java
+++ src/BankApp.java
@@ -47,4 +47,85 @@ public class BankApp extends Application implements BankProcess {
             }
         };
     }
+
+    /**
+     * Get the balance of the Account number
+     *
+     * @param AccountID Unique Identifier of Account
+     * @return Amount of money
+     */
+    @Override
+    public double getBalance(int AccountID) {
+        return 0;
+    }
+
+    /**
+     * Add the funds to the specified account number
+     *
+     * @param AccountID Unique Identifier of Account
+     * @param amount    Amount of Money
+     */
+    @Override
+    public void addFunds(int AccountID, double amount) {
+
+    }
+
+    /**
+     * Removes the funds from the given account
+     *
+     * @param AccountID Unique Identifier of Account
+     * @param amount    Amount of Money
+     */
+    @Override
+    public void removeFunds(int AccountID, double amount) {
+
+    }
+
+    /**
+     * Locks a certain amount of money away for potential use, returns an
+     * integer value that can be used for later use.
+     *
+     * @param AccountID Unique Identifier of Account
+     * @param amount    Amount of Money
+     * @return Random/Unique Integer of lock, for later retrieval
+     */
+    @Override
+    public int lockFunds(int AccountID, double amount) {
+        return 0;
+    }
+
+    /**
+     * Unlocks the lock given by the identifier!
+     *
+     * @param AccountID Unique Identifier of Account
+     * @param lockID    Identifier of the lock
+     */
+    @Override
+    public void unlockFunds(int AccountID, int lockID) {
+
+    }
+
+    /**
+     * Transfer funds of amount specified from ID1 to ID2
+     *
+     * @param fromID Unique Identifier of Account1
+     * @param toID   Unique Identifier of Account2
+     * @param amount Amount of Money
+     */
+    @Override
+    public void transferFunds(int fromID, int toID, double amount) {
+
+    }
+
+    /**
+     * Transfer funds based on the lock within the account tied to the fromID
+     *
+     * @param fromID Unique Identifier of Account1
+     * @param toID   Unique Identifier of Account2
+     * @param lockID Lock identifier
+     */
+    @Override
+    public void transferFunds(int fromID, int toID, int lockID) {
+
+    }
 }
