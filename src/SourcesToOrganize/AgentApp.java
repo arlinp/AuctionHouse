1c7cbb0abe5ebee6680c532a1190902542384be3
Gavin McGuire <mcguireg@unm.edu>
2019-04-25 11:08:31 -0600

    implemented processes for Agent

--- src/SourcesToOrganize/AgentApp.java
+++ src/SourcesToOrganize/AgentApp.java
@@ -184,10 +184,11 @@ public class AgentApp extends Application{
 
             localTest();
 
-            window.setScene(agentScene());
+            window.setScene(bankAccountScene());
         });
 
 
+        //new account taking input text
         TextField bankHostInput = new TextField("Bank Host");
         TextField auctionHostInput = new TextField("AuctionHost");
 
@@ -213,7 +214,7 @@ public class AgentApp extends Application{
 
     }
 
-    private Scene agentScene() {
+    private Scene bankAccountScene() {
 
         GridPane root = new GridPane();
 
@@ -251,140 +252,8 @@ public class AgentApp extends Application{
 
     public static void main(String[] args) {
 
-//        launch(args);
-        AgentApp app = new AgentApp();
-
-
-//        boolean test = false;
-//
-//        Scanner inScanner = new Scanner(System.in);
-//        String input;
-//
-//        System.out.println("1. Test on own PC?");
-//        System.out.println("2. Test in CS lab?");
-//
-//
-//        input = inScanner.nextLine();
-//
-//        //Initialize the bank and auction proxy
-//        BankProxy bankProxy = null;
-//        AuctionProxy auctionProxy = null;
-//
-//        try {
-//
-//
-//            if (input.equals("1")) {
-//
-//                bankProxy = new BankProxy("localHost", 42069, this);
-//                auctionProxy = new AuctionProxy("localhost", 42070);
-//
-//            }
-//
-//            if (input.equals("2")) {
-//
-//                System.out.println("enter bank host:");
-//                input = inScanner.nextLine();
-//                bankProxy = new BankProxy(input, 42069, this);
-//
-//                System.out.println("enter auctionhouse host:");
-//                input = inScanner.nextLine();
-//                auctionProxy = new AuctionProxy(input, 42070);
-//
-//            }
-//
-//            if (bankProxy == null) throw new ConnectException();
-//
-//            if (auctionProxy == null) throw new ConnectException();
-//
-//        } catch (ConnectException e) {
-//
-//
-//        }
-//
-//        //int accountID = bankProxy.addAccount();
-//        int accountID = 0;
-//
-//        //main loop
-//        while (!input.equals("exit")) {
-//
-//            //access bank or access auction
-//            System.out.println("what would you like to do?");
-//            System.out.println("1. Access bank.");
-//            System.out.println("2. Access auction house.");
-//            System.out.println("exit. to end program");
-//
-//            input = inScanner.nextLine();
-//
-//            //if bank, get balance
-//            if (input.equals("1")) {
-//
-//                System.out.println("Your Account ID is: ");
-//                System.out.println(" or nothing...");
-//                input = inScanner.nextLine();
-//
-//                if (input.equals("")) {
-//                    accountID = 5;
-//                } else {
-//                    accountID = Integer.parseInt(input);
-//                }
-//
-//                accountID = bankProxy.addAccount(accountID);
-//
-//
-//                System.out.println("Account ID=" + accountID);
-//                System.out.println("account balance = "
-//                        + bankProxy.getBalance(accountID));
-//
-//                while (!input.equals("done")) {
-//
-//                    System.out.println("1. add to funds.");
-//                    System.out.println("done. to exit bank");
-//                    input = inScanner.nextLine();
-//
-//                    if (input.equals("1")) {
-//
-//                        System.out.println("Enter amount to deposit:");
-//                        input = inScanner.nextLine();
-//
-//                        bankProxy.addFunds(accountID, Integer.parseInt(input));
-//
-//                        System.out.println("New Balance: " + bankProxy.getBalance(accountID));
-//                    }
-//
-//                }
-//
-//            }
-//
-//            //if auction, make bid
-//            if (input.equals("2")) {
-//
-//                System.out.println("Items for auction: ");
-//                ArrayList<ItemInfo> auctionItems = auctionProxy.getItems();
-//
-//                System.out.println("enter number to make bid");
-//
-//                for (int i = 0; i < auctionItems.size(); i++) {
-//                    System.out.println("" + i + " : " + auctionItems.get(i));
-//                }
-//
-//                input = inScanner.nextLine();
-//
-//                System.out.println("Bid attempt: " + input);
-//                int itemIndex = Integer.parseInt(input);
-//                System.out.println();
-//
-//                System.out.println("enter amount: ");
-//                input = inScanner.nextLine();
-//                double amount = Double.parseDouble(input);
-//
-////                System.out.println("making bid on: " + auctionItems.get(itemIndex) + " " +
-////                        "for : " + amount);
-////
-////                auctionProxy.bid(new Bid(amount,accountID,auctionItems.get(itemIndex).getItemID()));
-//
-//                auctionProxy.bid(new Bid(amount, accountID, itemIndex));
-//            }
-//        }
+        launch(args);
+//        AgentApp app = new AgentApp();
 
     }
 
