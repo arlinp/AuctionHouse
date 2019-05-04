6d5943384ed2f074310b8161845e2e6ed74afe2b
arlin <arlin@cs.unm.edu>
2019-05-04 15:42:42 -0600

    AuctionRequest cleaned and commented.

--- src/AuctionProxy/AuctionRequest.java
+++ src/AuctionProxy/AuctionRequest.java
@@ -6,6 +6,11 @@ import SourcesToOrganize.Packet;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * An AuctionRequest object saves the
+ * the status of an auction and serializes
+ * the request.
+ */
 public class AuctionRequest extends Packet implements Serializable {
 
     // Things that are going to be send and returned
@@ -20,83 +25,139 @@ public class AuctionRequest extends Packet implements Serializable {
     private double newAmount = 0;
 
 
+    /**
+     * @param type Type of request
+     */
     public AuctionRequest(AuctionInfo type) {
         this.type = type;
         setPacketID((int)(Math.random()*Integer.MAX_VALUE));
         setStatus(true);
     }
 
+    /**
+     * @param type type of request
+     * @param ID request ID
+     */
     public AuctionRequest(AuctionInfo type, int ID) {
         this.type = type;
         setPacketID(ID);
         setStatus(true);
     }
 
-
+    /**
+     * @return item ID
+     */
     public int getItemID() {
         return itemID;
     }
 
+    /**
+     * @param itemID ID to assign
+     */
     public void setItemID(int itemID) {
         this.itemID = itemID;
     }
 
+    /**
+     * @return type of auction request
+     */
     public AuctionInfo getType() { return type; }
 
+    /**
+     * @return info of item being auctioned
+     */
     public ItemInfo getItem() {
         return itemInfo;
     }
 
+    /**
+     * @return list of active items
+     */
     public ArrayList<ItemInfo> getItems() {
         return items;
     }
 
+    /**
+     * @param bid bid to set
+     */
     public void setBid(Bid bid) {
         this.bid = bid;
     }
 
+    /**
+     * @return bid
+     */
     public Bid getBid() {
         return bid;
     }
 
+    /**
+     * @param contains boolean
+     */
     public void setContains(boolean contains) {
         this.contains = contains;
     }
 
+    /**
+     * @param itemInfo item info to save
+     */
     public void setItemInfo(ItemInfo itemInfo) {
         this.itemInfo = itemInfo;
     }
 
+    /**
+     * @param items List of items to save
+     */
     public void setItems(ArrayList<ItemInfo> items) {
         if (items == null) this.items = null;
         else this.items = items;
     }
 
-
+    /**
+     * @return message being sent
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * @param message message to save
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * @return status of bid
+     */
     public BidInfo getBidStatus() {
         return bidStatus;
     }
 
+    /**
+     * @param bidStatus set status of bid
+     */
     public void setBidStatus(BidInfo bidStatus) {
         this.bidStatus = bidStatus;
     }
 
+    /**
+     * @return get new amount of bid
+     */
     public double getNewAmount() {
         return newAmount;
     }
 
+    /**
+     * @param newAmount set the new amount of the bid
+     */
     public void setNewAmount(double newAmount) {
         this.newAmount = newAmount;
     }
 
+    /**
+     * @return checks if contains is true/false
+     */
     public boolean isContains() {
         return contains;
     }
