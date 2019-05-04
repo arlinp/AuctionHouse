ae87cfca9d59ff6071768973de1a16a818b9e826
arlin <arlin@cs.unm.edu>
2019-05-04 14:45:55 -0600

    Agent commented.

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -31,8 +31,8 @@ public class Agent implements BankProcess, AuctionProcess {
 
     /**
      * connect to the bank to make an account and receive auction info
-     * @param bankHost
-     * @param bankPort
+     * @param bankHost host name
+     * @param bankPort port number
      */
     public Agent(String bankHost, int bankPort){
 
@@ -50,19 +50,36 @@ public class Agent implements BankProcess, AuctionProcess {
         return accountID;
     }
 
+    /**
+     * Getter for auction proxies
+     * @return arraylist of all connected auctions proxies
+     */
     public ArrayList<AuctionProxy> getConnectedAuctionProxys() {
         return connectedAuctionProxys;
     }
 
+    /**
+     * @return returns list of current bids
+     */
     public LinkedList<Bid> getBids() {
         return bids;
     }
 
+    /**
+     * Makes an account for auction house or agent
+     *
+     * @return Account ID
+     */
     @Override
     public int addAccount(int ID) {
         return 0;
     }
 
+    /**
+     * To place a bid
+     *
+     * @param bid Bid object that contains elements
+     */
     @Override
     public BidInfo bid(Bid bid) {
         addToBids(bid);
@@ -70,19 +87,38 @@ public class Agent implements BankProcess, AuctionProcess {
         return info;
     }
 
+    /**
+     * Gets the Item Info from the Item ID.
+     *
+     * @param itemID Identifier of Item
+     * @return ItemInfo
+     */
     @Override
     public ItemInfo getItemInfo(int itemID) {
         return currentAuctionProxy.getItemInfo(itemID);
     }
 
+    /**
+     * Synchronized way of adding a bid to list
+     * @param bid the bid to add
+     */
     public synchronized void addToBids(Bid bid){
         bids.add(bid);
     }
 
+    /**
+     * Synchronized way o fremoving a bid from bid list
+     * @param bid the bid to remove
+     */
     public synchronized void removeBid(Bid bid){
         bids.remove(bid);
     }
 
+    /**
+     * Gets an ArrayList of all of the Items
+     *
+     * @return ArrayList of Items
+     */
     @Override
     public synchronized ArrayList<ItemInfo> getItems() {
         ArrayList<ItemInfo> itemInfos = new ArrayList<>();
@@ -95,6 +131,10 @@ public class Agent implements BankProcess, AuctionProcess {
 
     }
 
+    /**
+     * Connect to all the auction proxies that bank has let
+     * us know about
+     */
     public void connectToAuctions(){
 
         for ( NetworkDevice n : bankProxy.getServers()){
@@ -103,11 +143,22 @@ public class Agent implements BankProcess, AuctionProcess {
         }
     }
 
+    /**
+     * @param auctionProxy Auction to add
+     * @return was it able to be added
+     */
     public boolean addAuctionProxy(AuctionProxy auctionProxy){
         connectedAuctionProxys.add(auctionProxy);
         return true;
     }
 
+    /**
+     * Add a proxy based on hostname and port
+     *
+     * @param host host name
+     * @param port port number
+     * @return
+     */
     public boolean addAuctionProxy(String host, int port){
 
         try{
@@ -132,7 +183,7 @@ public class Agent implements BankProcess, AuctionProcess {
 
 
     /**
-     * Gets balance
+     * Gets balance of specific ID
      *
      * @param AccountID Unique Identifier of Account
      * @return balance of account
@@ -143,6 +194,9 @@ public class Agent implements BankProcess, AuctionProcess {
     }
 
     /**
+     * Get balance method used to get balance
+     * of an Agent instance
+     *
      * @return balance
      */
     public double getBalance() {
@@ -151,9 +205,10 @@ public class Agent implements BankProcess, AuctionProcess {
 
 
     /**
+     * Add the funds to the specified account number
+     *
      * @param AccountID Unique Identifier of Account
-     * @param amount    Amount of Money
-     * @return
+     * @param amount Amount of Money
      */
     @Override
     public boolean addFunds(int AccountID, double amount) {
@@ -163,28 +218,60 @@ public class Agent implements BankProcess, AuctionProcess {
         return bankProxy.addFunds(accountID, amount);
     }
 
-
-
+    /**
+     * Removes the funds from the given account
+     *
+     * @param AccountID Unique Identifier of Account
+     * @param amount Amount of Money
+     */
     @Override
     public boolean removeFunds(int AccountID, double amount) {
         return bankProxy.removeFunds(AccountID, amount);
     }
 
+    /**
+     * Locks a certain amount of money away for potential use, returns an
+     * integer value that can be used for later use.
+     *
+     * @param AccountID Unique Identifier of Account
+     * @param amount Amount of Money
+     * @return Random/Unique Integer of lock, for later retrieval
+     */
     @Override
     public int lockFunds(int AccountID, double amount) {
         return bankProxy.lockFunds(AccountID, amount);
     }
 
+    /**
+     * Unlocks the lock given by the identifier!
+     *
+     * @param AccountID Unique Identifier of Account
+     * @param lockID Identifier of the lock
+     */
     @Override
     public boolean unlockFunds(int AccountID, int lockID) {
         return bankProxy.unlockFunds(AccountID, lockID);
     }
 
+    /**
+     * Transfer funds of amount specified from ID1 to ID2
+     *
+     * @param fromID Unique Identifier of Account1
+     * @param toID Unique Identifier of Account2
+     * @param amount Amount of Money
+     */
     @Override
     public boolean transferFunds(int fromID, int toID, double amount) {
         return bankProxy.transferFunds(fromID,toID, amount);
     }
 
+    /**
+     * Transfer funds based on the lock within the account tied to the fromID
+     *
+     * @param fromID Unique Identifier of Account1
+     * @param toID Unique Identifier of Account2
+     * @param lockID Lock identifier
+     */
     @Override
     public boolean transferFunds(int fromID, int toID, int lockID) {
         return bankProxy.transferFunds(fromID, toID,lockID);
@@ -222,20 +309,34 @@ public class Agent implements BankProcess, AuctionProcess {
         return bankProxy.getServers();
     }
 
-
+    /**
+     * Get account number
+     *
+     * @return Account number
+     */
     public int getAccountID() {
         return accountID;
     }
 
+    /**
+     * Set the account number of agent
+     *
+     * @param accountID Account number
+     */
     public void setAccountID(int accountID) {
         this.accountID = accountID;
     }
 
-
+    /**
+     * @return LinkedBlockingQueue of connected auction devices
+     */
     public LinkedBlockingQueue<NetworkDevice> getConnectedAuctionNetworkDevices() {
         return connectedAuctionNetworkDevices;
     }
 
+    /**
+     * @param auction Network device of auction to add
+     */
     public void addAuctionHouse(NetworkDevice auction) {
 
         System.out.println("add auction");
