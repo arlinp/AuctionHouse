2dacd192cb8a7f7ed3abff8ecf732ea1e267ee52
arlin <arlin@cs.unm.edu>
2019-05-04 15:50:30 -0600

    BidInfo cleaned and commented.

--- src/AuctionProxy/BidInfo.java
+++ src/AuctionProxy/BidInfo.java
@@ -1,5 +1,10 @@
 package AuctionProxy;
 
+/**
+ * Enumeration of bid statuses
+ * Used by AuctionHouse and AuctionProxy
+ * to manage information about bids
+ */
 public enum BidInfo {
     ACCEPTANCE, REJECTION, OUTBID, WINNER;
 }
