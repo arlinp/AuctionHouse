fbefb36535773de17355c15ddd5b44a5d9fb70f5
arlin <arlin@cs.unm.edu>
2019-05-04 16:17:54 -0600

    BankRequest cleaned and commented.

--- src/BankProxy/BankRequest.java
+++ src/BankProxy/BankRequest.java
@@ -8,6 +8,10 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
+/**
+ * Saves a bank request as an object with
+ * information about the request
+ */
 public class BankRequest extends Packet implements Serializable {
 
     private BankInfo type;
@@ -19,94 +23,135 @@ public class BankRequest extends Packet implements Serializable {
     private boolean isSuccess;
     private LinkedBlockingQueue<NetworkDevice> networkDevices;
 
+    /**
+     * Constructs a request with a packetID
+     * and list of network devices
+     *
+     * @param type type of request
+     */
     public BankRequest(BankInfo type) {
         setPacketID((int)(Math.random()*Integer.MAX_VALUE));
         networkDevices = new LinkedBlockingQueue<>();
         this.type = type;
     }
 
-    // TODO Check usage
-    public BankRequest() {
-            setStatus(true);
-    }
-
+    /**
+     * Constructs a request with a given packetID
+     * and list of network devices
+     *
+     * @param type type of request
+     */
     public BankRequest(BankInfo type, int ID) {
         setPacketID((int)(Math.random()*Integer.MAX_VALUE));
         this.type = type;
         setPacketID(ID);
     }
 
-    public void setSuccess(boolean success) {
-        isSuccess = success;
-    }
-
-    public boolean isSuccess() {
-        return isSuccess;
-    }
-
-    public int getLock() {
-        return 0;
-    }
-
+    /**
+     * @return type of request
+     */
     public BankInfo getType() {
         return type;
     }
 
+    /**
+     * @return request ID
+     */
     public int getID() {
         return ID;
     }
 
+    /**
+     * @param ID set this request's ID
+     */
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    /**
+     * @return ID of who the message is going to
+     */
     public int getToID() {
         return ID1;
     }
 
+    /**
+     * @param ID1 set ID of who the message is going to
+     */
     public void setToID(int ID1) {
         this.ID1 = ID1;
     }
 
+    /**
+     * @return get the amount that is part of some account requests
+     */
     public double getAmount() {
         return amount;
     }
 
+    /**
+     * set amount requested
+     * @param amount amount to set
+     */
     public void setAmount(double amount) {
         this.amount = amount;
     }
 
+    /**
+     * @return get the lock
+     */
     public int getLockNumber() {
         return lockNumber;
     }
 
+    /**
+     * @param lockNumber set the lock ID
+     */
     public void setLockNumber(int lockNumber) {
         this.lockNumber = lockNumber;
     }
 
+    /**
+     * @return list of network devices
+     */
     public LinkedBlockingQueue<NetworkDevice> getNetworkDevices() {
         return networkDevices;
     }
 
+    /**
+     * @param networkDevices set the network devices
+     */
     public void setNetworkDevices(LinkedBlockingQueue<NetworkDevice> networkDevices) {
         this.networkDevices = networkDevices;
     }
 
+    /**
+     * @return get all network devices
+     */
     public NetworkDevice getNetworkDevice() {
         if (networkDevices.size() > 0) return networkDevices.peek();
         else return null;
     }
 
+    /**
+     * Add a new network device to list using ip and port #
+     *
+     * @param ipAddress address of new network device
+     * @param port new network device port
+     */
     public void addNetworkDevices(String ipAddress, int port) {
         NetworkDevice networkDevice = new NetworkDevice(ipAddress, port);
         networkDevices.add(networkDevice);
     }
 
+    /**
+     * Add a new network device object to list
+     *
+     * @param networkDevice network device to add
+     */
     public void addNetworkDevices(NetworkDevice networkDevice) {
         if (networkDevices == null) networkDevices = new LinkedBlockingQueue<>();
         networkDevices.add(networkDevice);
     }
-
-
 
 }
