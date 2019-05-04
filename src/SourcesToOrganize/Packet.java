ac12f14ec5223da8c0a126df1a15118db1119cf2
arlin <arlin@cs.unm.edu>
2019-05-04 16:44:20 -0600

    Packet class started to comment but don't understand.

--- src/SourcesToOrganize/Packet.java
+++ src/SourcesToOrganize/Packet.java
@@ -2,6 +2,9 @@ package SourcesToOrganize;
 
 import java.io.Serializable;
 
+/**
+ *
+ */
 public class Packet implements Serializable {
 
     private Boolean status = false;
@@ -9,27 +12,45 @@ public class Packet implements Serializable {
     private int packetID = 0;
     // Troubleshooting stuff goes here
 
+    /**
+     * @param status
+     */
     public void setStatus(Boolean status) {
         this.status = status;
     }
 
+    /**
+     * @return
+     */
     public Boolean getStatus() {
         return status;
     }
 
 
+    /**
+     * @return
+     */
     public Boolean getAck() {
         return ack;
     }
 
+    /**
+     * @param ack
+     */
     public void setAck(Boolean ack) {
         this.ack = ack;
     }
 
+    /**
+     * @return
+     */
     public int getPacketID() {
         return packetID;
     }
 
+    /**
+     * @param packetID
+     */
     public void setPacketID(int packetID) {
         this.packetID = packetID;
     }
