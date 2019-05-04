0fab9bf9e2145988bd7f718ff0534a7ca2552a55
arlin <arlin@cs.unm.edu>
2019-05-04 16:42:10 -0600

    NetworkDevice commented and cleaned.

--- src/SourcesToOrganize/NetworkDevice.java
+++ src/SourcesToOrganize/NetworkDevice.java
@@ -2,28 +2,52 @@ package SourcesToOrganize;
 
 import java.io.Serializable;
 
+/**
+ * NetworkDevice is an object with information
+ * on a connection
+ * -IP Address
+ * -Port number
+ */
 public class NetworkDevice implements Serializable {
 
     private String ipAddress;
     private int port;
 
+    /**
+     * Constructs a new connection
+     *
+     * @param ipAddress ip of connection
+     * @param port port number
+     */
     public NetworkDevice(String ipAddress, int port) {
         this.ipAddress = ipAddress;
         this.port = port;
     }
 
+    /**
+     * @return ip address of the connection
+     */
     public String getIpAddress() {
         return ipAddress;
     }
 
+    /**
+     * @param ipAddress ip address to be assigned
+     */
     public void setIpAddress(String ipAddress) {
         this.ipAddress = ipAddress;
     }
 
+    /**
+     * @return port number
+     */
     public int getPort() {
         return port;
     }
 
+    /**
+     * @param port port number to be set
+     */
     public void setPort(int port) {
         this.port = port;
     }
@@ -33,6 +57,10 @@ public class NetworkDevice implements Serializable {
         return ipAddress + ":" + port;
     }
 
+    /**
+     * @param obj Checks for object equality
+     * @return
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null || obj.getClass() != getClass()) {
